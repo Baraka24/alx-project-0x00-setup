12192259694899f7a22c3857fa966477d216c1d8
Baraka Kinywa <bkinywa24@gmail.com>
2025-11-11 13:48:19 +0200

    functional components

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -7,7 +7,7 @@ interface PillProps {
 const Pill: React.FC<PillProps> = ({ title }) => {
     return (
     <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
-      <p className=" text-sm ">{title}</p>
+      <p className=" text-sm text-[#000000] ">{title}</p>
     </div>
   )
 }
